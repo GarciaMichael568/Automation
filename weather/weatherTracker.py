871fbfe1c6dc6e1db6f27ee7543ad0210018965f
Michael Garcia <garciamichael.developer@gmail.com>
2022-03-14 19:04:24 -0600

    retrieves temp and description

--- weather/weatherTracker.py
+++ weather/weatherTracker.py
@@ -4,11 +4,15 @@ API_KEY = "73ba7c7ca5fbd5bde79ba42b167c03c2"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city: ")
-request_url = f"{BASE_URL}?q={city}&appid={API_KEY}"
+request_url = f"{BASE_URL}?q={city}&units=imperial&appid={API_KEY}"
 response = requests.get(request_url)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
+    #newprint(data)
+    weather = data['weather'][0]["description"]
+    print(f"Weather is {weather}.")
+    temp = data['main']["temp"]
+    print(f"Temperature: {temp}F.")
 else:
     print("Error occured during API call.")
